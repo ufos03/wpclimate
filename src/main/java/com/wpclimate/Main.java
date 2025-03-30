13e3cafd80ad4391a52fce81f69ceb0e660c914d
ufos <zampilliantonio.20@gmail.com>
2025-03-30 15:05:12 +0200

    Refactor Main class to update WpCli instantiation with an empty string parameter and correct exception import paths

--- src/main/java/com/wpclimate/Main.java
+++ src/main/java/com/wpclimate/Main.java
@@ -1,10 +1,10 @@
 package com.wpclimate;
 import com.wpclimate.Cli.WpCli;
-import com.wpclimate.Cli.exceptions.PHPNotInstalledException;
-import com.wpclimate.Cli.exceptions.WPCliNotInstalledException;
+import com.wpclimate.Cli.Exceptions.PHPNotInstalledException;
+import com.wpclimate.Cli.Exceptions.WPCliNotInstalledException;
 
 public class Main {
     public static void main(String[] args) throws PHPNotInstalledException, WPCliNotInstalledException {
-        WpCli wp = new WpCli();
+        WpCli wp = new WpCli("");
     }
 }
