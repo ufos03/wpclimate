36bb8733de3da65ff036c1110d1bcc88155d896c
ufos <zampilliantonio.20@gmail.com>
2025-03-31 15:47:40 +0200

    Update WpCli instantiation with a specific file path and add search/replace functionality

--- src/main/java/com/wpclimate/Main.java
+++ src/main/java/com/wpclimate/Main.java
@@ -4,7 +4,10 @@ import com.wpclimate.Cli.Exceptions.PHPNotInstalledException;
 import com.wpclimate.Cli.Exceptions.WPCliNotInstalledException;
 
 public class Main {
-    public static void main(String[] args) throws PHPNotInstalledException, WPCliNotInstalledException {
-        WpCli wp = new WpCli("");
+    public static void main(String[] args) throws PHPNotInstalledException, WPCliNotInstalledException 
+    {
+        WpCli wp = new WpCli("/home/ufos/Documents/test-wpclimate/");
+        wp.doSearchReplace("http://test.local", "http://test2.local", true, true);
+        wp.doFlushTransient();
     }
 }
