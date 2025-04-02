2efa9621d907f986f6807b541528984709856e2e
ufos <zampilliantonio.20@gmail.com>
2025-04-02 22:13:35 +0200

    Enable output display in WpCli for search and replace operations

--- src/main/java/com/wpclimate/Main.java
+++ src/main/java/com/wpclimate/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws PHPNotInstalledException, WPCliNotInstalledException 
     {
         WpCli wp = new WpCli("/home/ufos/Documents/test-wpclimate/");
-        wp.setShowOutput(false);
+        wp.setShowOutput(true);
         wp.doSearchReplace("http://test.local", "http://test2.local", true, true);
         wp.setShowOutput(true);
         wp.doFlushTransient();
