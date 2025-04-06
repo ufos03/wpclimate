aec1961441df575ebfd0eca6d64ca5ff86fbb7c0
ufos <zampilliantonio.20@gmail.com>
2025-04-06 20:36:45 +0200

    Add environment variable support to executeCommand methods in Shell interface

--- src/main/java/com/wpclimate/shell/Shell.java
+++ src/main/java/com/wpclimate/shell/Shell.java
@@ -1,5 +1,7 @@
 package com.wpclimate.shell;
 
+import java.util.Map;
+
 /**
  * The Shell interface defines methods for executing shell commands.
  */
@@ -13,12 +15,23 @@ public interface Shell
      */
     CommandOutput executeCommand(String commandLine);
 
+
+    /**
+     * Executes a shell command in a specified directory.
+     *
+     * @param commandLine The command line to execute.
+     * @param environment A map of environment variables to set for the command.
+     * @return A CommandOutput object containing the standard output and error output of the command.
+     */
+    CommandOutput executeCommand(String commandLine, Map<String, String> environment);
+
     /**
      * Executes a shell command in a specified directory.
      *
      * @param commandLine The command line to execute.
      * @param directory The directory in which to execute the command.
+     * @param environment A map of environment variables to set for the command.
      * @return A CommandOutput object containing the standard output and error output of the command.
      */
-    CommandOutput executeCommand(String commandLine, String directory);
+    CommandOutput executeCommand(String commandLine, String directory, Map<String, String> environment);
 }
