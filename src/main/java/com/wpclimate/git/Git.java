842773ff354f27258e55a7eb52de18704863c8b5
ufos <zampilliantonio.20@gmail.com>
2025-04-15 15:55:49 +0200

    Refactor Git constructor to initialize dependencies and configure HttpsCredentials

--- src/main/java/com/wpclimate/git/Git.java
+++ src/main/java/com/wpclimate/git/Git.java
@@ -1,13 +1,34 @@
 package com.wpclimate.git;
 
+import java.util.Map;
+
+import com.wpclimate.configurator.Configurator;
+import com.wpclimate.constants.FileManager;
+import com.wpclimate.constants.FileName;
+import com.wpclimate.git.core.Dependency;
+import com.wpclimate.git.core.GitContext;
+import com.wpclimate.git.credentials.https.HttpsCredentials;
 import com.wpclimate.shell.Command;
 import com.wpclimate.shell.Shell;
 
 public class Git 
 {
-    public Git()
+    public Git() throws Exception
     {
-        Shell shell = new Command();
-        System.out.println(shell.executeCommand("git --version").getStandardOutput());
+        FileManager flm = new FileManager();
+        Shell shl = new Command(flm.getWorkingDirectory().getAbsolutePath());
+        Dependency gtd = new Dependency(shl);
+        Configurator cfg = new com.wpclimate.configurator.MultiFileConfiguration();
+        GitContext ctx = new GitContext(shl, flm, gtd, cfg);
+        HttpsCredentials dd = new HttpsCredentials(ctx);
+        dd.configure(
+            Map.of
+            (
+                "name", "test",
+                "url", "dd",
+                "username", "ufos",
+                "password", "dd"
+            ));
+        System.out.println(dd.read().toString());
     }
 }
